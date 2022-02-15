b3fd919886afa45bbc6aa4a0c193f61f4514d3eb
Francisbellajay <francisbell.ajay@gmail.com>
2022-02-15 17:50:03 +0530

    Create Excerise1Product.java
    updated

---
+++ Firstapp/src/qaace22training/Excerise1Product.java
@@ -0,0 +1,13 @@
+package qaace22training;
+
+public class Excerise1Product {
+
+	public static void main(String[] args) {
+		 int a = 10;
+         int b = 15;
+         int c = a*b;
+         System.out.println("The product is " + c);
+
+	}
+
+}
