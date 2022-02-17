3aca639cbbcdb88798ea6051507c297618c5eacf
Francisbellajay <francisbell.ajay@gmail.com>
2022-02-17 15:55:05 +0530

    Day-4-exercise

---
+++ Firstapp/src/qaace22training/countinstance.java
@@ -0,0 +1,35 @@
+package qaace22training;
+
+public class countinstance {
+
+
+	private static int count;
+    public countinstance ()   
+     {
+        count++;
+     }
+    public static void main(String[] args) {
+
+
+
+		// TODO Auto-generated method stub
+
+        countinstance  ob1 = new countinstance ();
+        countinstance  ob2 = new countinstance ();
+        countinstance  ob3 = new countinstance ();
+        countinstance  ob4 = new countinstance ();
+        countinstance  ob5 = new countinstance ();
+        countinstance  ob6 = new countinstance ();
+        countinstance  ob7 = new countinstance ();
+
+
+      System.out.print("Total Number of Objects: " + countinstance .count);
+
+
+
+	}
+
+
+
+}
+
